05362c93972fa8dcdbb8fb51ca453eedf8a997cd
John Berry <ulfmagnetics@gmail.com>
2016-09-05 02:35:16 +0000

    Polish matrix code a bit
    - Set colors conditionally based on ETA
    - Spawn a thread to loop over arrivals until polling interval expires

--- pghnextrgbus/core.py
+++ pghnextrgbus/core.py
@@ -1,19 +1,23 @@
 from envparse import env
 from locator import Locator
-from matrix import Matrix
+from matrix import Renderer, Matrix
 import time
 import signal
 import sys
 
 matrix = None
+renderer = None
 
 def sigint_handler(signal, frame):
-    global matrix
+    global matrix, renderer
     if matrix:
         matrix.clear()
+    if renderer:
+        renderer.join(1.0)
     sys.exit(0)
 
 def main(args):
+    global matrix, renderer
     poll_interval = env('POLL_INTERVAL', default=30, cast=int)
     api_key       = env('API_KEY')
     stop_id       = env('STOP_ID')
@@ -21,17 +25,16 @@ def main(args):
     matrix_rows   = env('MATRIX_ROWS', default=16, cast=int)
     matrix_chain_length = env('MATRIX_CHAIN_LENGTH', default=1, cast=int)
 
-    global matrix
     matrix = Matrix(rows=matrix_rows, chain_length=matrix_chain_length)
     locator = Locator(api_key=api_key, stop_id=stop_id, direction=direction)
-
     signal.signal(signal.SIGINT, sigint_handler)
+
+    renderer = Renderer(matrix)
+    renderer.daemon = True
     while True:
-        # FIXME need to loop over all arrivals until poll interval expires
-        for arrival in locator.next_arrivals():
-            matrix.clear()
-            matrix.render_arrival(arrival)
-            time.sleep(5)
+        renderer.arrivals = locator.next_arrivals()
+        if not renderer.is_alive():
+            renderer.start()
         time.sleep(poll_interval)
 
 if __name__ == '__main__':
--- pghnextrgbus/matrix.py
+++ pghnextrgbus/matrix.py
@@ -1,6 +1,31 @@
 from envparse import env
 from datetime import timedelta
 from rgbmatrix import Adafruit_RGBmatrix
+import threading
+import time
+
+# TODO these should be exposed as config variables
+URGENCY_COLORS = {'green': 15, 'yellow': 10, 'red': 5}
+
+class Renderer(threading.Thread):
+    def __init__(self, matrix, delay=5):
+        super(Renderer, self).__init__()
+        self.matrix = matrix
+        self.delay = delay
+        self._arrivals = []
+
+    @property
+    def arrivals(self): return self._arrivals
+
+    @arrivals.setter
+    def arrivals(self, value): self._arrivals = value
+
+    def run(self):
+        while True:
+            for arrival in self._arrivals:
+                self.matrix.clear()
+                self.matrix.render_arrival(arrival)
+                time.sleep(self.delay)
 
 class Matrix(object):
     def __init__(self, rows=16, chain_length=1, font_width='5', font_height='8'):
@@ -17,17 +42,29 @@ class Matrix(object):
         self.debug(arrival)
         eta = arrival.eta()
         if eta < timedelta(minutes=1):
+            minutes = 1
             eta_text = "<1 min!"
         else:
             minutes = eta.seconds / 60
             eta_text = "{0} min".format(minutes)
-        self.render_line(1, arrival.route, 0xff, 0, 0xff)
-        # TODO conditional colors based on ETA urgency!
-        self.render_line(2, eta_text, 0xff, 0xff, 0)
+        self.render_line(1, arrival.route, 0xff00ff)
+        self.render_line(2, eta_text, self.urgency_color(minutes))
 
-    def render_line(self, line_num, text, r, g, b):
+    def render_line(self, line_num, text, rgb):
         y_offset = (line_num - 1) * self.font_height
+        r = (rgb >> 16) & 0xff
+        g = (rgb >>  8) & 0xff
+        b = (rgb >>  0) & 0xff
         self.rgbmatrix.DrawText(self.bdf_font_file, 1, y_offset, r, g, b, text)
+
+    def urgency_color(self, minutes):
+        global URGENCY_COLORS
+        if minutes >= URGENCY_COLORS['green']:
+            return 0x00ff00
+        elif minutes >= URGENCY_COLORS['yellow']:
+            return 0xffff00
+        else:
+            return 0xff0000
 
     def debug(self, msg):
         if env('DEBUG', default=False, cast=bool):
