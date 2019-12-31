4c9f4b5f6ec2dc728188ca4e545560122ff6f372
John Berry <ulfmagnetics@gmail.com>
2019-12-31 04:23:03 +0000

    Add timestamps to debug log statements

--- pghnextrgbus/utils.py
+++ pghnextrgbus/utils.py
@@ -1,6 +1,7 @@
 # This module contains utility methods that can be shared across the app
 from envparse import env
+from datetime import datetime
 
 def debug(msg):
     if env('DEBUG', default=False, cast=bool):
-        print msg
+        print("[%s] %s" % (datetime.now().strftime("%c"), msg))
